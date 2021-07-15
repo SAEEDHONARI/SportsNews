5e075dc49d15d96ab1cfc8952e096ea70bfb160a
Saeid_Honari <honari.1370@gmail.com>
2021-07-15 10:45:42 -0700

    Change Query Page Size to 20

--- app/src/main/java/com/example/baloot_saeedhonari/util/Constants.kt
+++ app/src/main/java/com/example/baloot_saeedhonari/util/Constants.kt
@@ -6,5 +6,5 @@ const val INSTAGRAM_LINK = "https://www.instagram.com/saeed__honari"
 const val GMAIL_LINK = "honari.1370@gmail.com"
 const val BASE_URL = "https://newsapi.org/v2/"
 const val SEARCH_NEWS_TIME_DELAY = 500L
-const val QUERY_PAGE_SIZE = 5
+const val QUERY_PAGE_SIZE = 20
 const val CATEGORY_NEWS = "sports"
